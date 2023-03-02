95faccd979c98f04311242f53f8e4d0f510f609e
dedanmsafari <dedan.msafari@gmail.com>
2023-03-02 16:46:08 +0300

    Swiper Component

---
+++ src/features/Account/components/Swiper.component.js
@@ -0,0 +1,18 @@
+import { SwiperContainerStyles } from "../screens/account.styles";
+import Text from "../../../components/text/text.component";
+import { Image } from "react-native";
+
+const SwiperContainer = ({ source, title, caption }) => {
+  console.log(source);
+  return (
+    <SwiperContainerStyles>
+      <Image style={{ width: 330, height: 330 }} source={source} />
+      <Spacer size="medium" />
+      <Text variant="walkthrough">{title}</Text>
+      <Spacer position="bottom" size="large" />
+      <Text variant="walkthroughCaption">{caption}</Text>
+    </SwiperContainerStyles>
+  );
+};
+
+export default SwiperContainer;
