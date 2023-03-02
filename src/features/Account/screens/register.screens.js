b1aacf7f2b993ad4a93e9f6c5377e69355e95ae5
dedanmsafari <dedan.msafari@gmail.com>
2023-03-02 16:46:51 +0300

    register screens

---
+++ src/features/Account/screens/register.screens.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const RegisterScreens = () => {
+  return <View>RegisterScreen</View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default RegisterScreens;
