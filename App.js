ec631b0aebaf4aa663ec37e1d52212e71b6580bf
dedanmsafari <dedan.msafari@gmail.com>
2023-03-02 16:45:29 +0300

    Chnaged app file

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useCallback } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./src/infrastructure/theme";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import {
+  useFonts as useLato,
+  Lato_300Light,
+  Lato_400Regular,
+  Lato_700Bold,
+} from "@expo-google-fonts/lato";
+import {
+  useFonts as useRoboto,
+  Roboto_300Light,
+  Roboto_400Regular,
+  Roboto_500Medium,
+} from "@expo-google-fonts/roboto";
+import AccountNavigator from "./src/infrastructure/navigation/account.navigator";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  let [LatoLoaded] = useLato({
+    Lato_300Light,
+    Lato_400Regular,
+    Lato_700Bold,
+  });
+
+  let [RobotoLoaded] = useRoboto({
+    Roboto_300Light,
+    Roboto_400Regular,
+    Roboto_500Medium,
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (LatoLoaded && RobotoLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [LatoLoaded, RobotoLoaded]);
+
+  if (!LatoLoaded || !RobotoLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <ThemeProvider theme={theme}>
+        <AccountNavigator onReady={onLayoutRootView} />
+        <ExpoStatusBar style="auto" />
+      </ThemeProvider>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
