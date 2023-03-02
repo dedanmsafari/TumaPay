e735ee7d18818c46aec02fff7dbeedc93877d030
dedanmsafari <dedan.msafari@gmail.com>
2023-03-02 16:47:17 +0300

    fonts

---
+++ src/infrastructure/theme/fonts.js
@@ -0,0 +1,25 @@
+export const fonts = {
+  light: "Lato_300Light",
+  body: "Lato_400Regular",
+  medium: "Roboto_500Medium",
+  heading: " Lato_700Bold",
+};
+
+export const fontWeights = {
+  light: 300,
+  regular: 400,
+  medium: 500,
+  bold: 700,
+};
+
+export const fontSizes = {
+  caption: "12px",
+  button: "14px",
+  body: "16px",
+  title: "20px",
+  h5: "24px",
+  h4: "30px",
+  h3: "45px",
+  h2: "56px",
+  h1: "112px",
+};
