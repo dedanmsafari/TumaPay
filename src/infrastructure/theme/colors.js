46582adacb0db7b2f0fb7b91d856445411cdaa79
dedanmsafari <dedan.msafari@gmail.com>
2023-03-02 16:47:10 +0300

    file colors

--- src/infrastructure/theme/colors.js
+++ src/infrastructure/theme/colors.js
@@ -6,7 +6,7 @@ export const colors = {
     // tertiary: "#138000",
   },
   ui: {
-    primary: "#262626",
+    primary: "#46494C",
     secondary: "#757575",
     tertiary: "#F1F1F1",
     quaternary: "#FFFFFF",
@@ -15,6 +15,7 @@ export const colors = {
     error: "#D0421B",
     success: "#138000",
     header: "#48AB48",
+    brand: "#006400",
   },
   bg: {
     primary: "#FFFFFF",
