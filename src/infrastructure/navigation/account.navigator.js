776169fafefcf4c43b154f1fe4db799662ae92ff
dedanmsafari <dedan.msafari@gmail.com>
2023-03-03 17:01:40 +0300

    account navigator

--- src/infrastructure/navigation/account.navigator.js
+++ src/infrastructure/navigation/account.navigator.js
@@ -5,7 +5,7 @@ import {
 } from "@react-navigation/stack";
 import AccountScreen from "../../features/Account/screens/account.screens";
 import LoginScreen from "../../features/Account/screens/login.screens";
-// import RegisterNavigator from "./register.navigator";
+import RegisterNavigator from "./register.navigator";
 import { NavigationContainer } from "@react-navigation/native";
 const Stack = createStackNavigator();
 
@@ -15,12 +15,11 @@ const AccountNavigator = ({ onReady }) => {
       <Stack.Navigator
         screenOptions={{
           headerShown: false,
-          ...TransitionPresets.SlideFromRightIOS,
         }}
       >
         <Stack.Screen name="Main" component={AccountScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
-        {/* <Stack.Screen name="Register" component={RegisterNavigator} /> */}
+        <Stack.Screen name="Register" component={RegisterNavigator} />
       </Stack.Navigator>
     </NavigationContainer>
   );
