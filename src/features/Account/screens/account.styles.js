4a13802555e83ecf014e279aa13e26894f6d789a
dedanmsafari <dedan.msafari@gmail.com>
2023-03-03 17:01:20 +0300

    account styles

--- src/features/Account/screens/account.styles.js
+++ src/features/Account/screens/account.styles.js
@@ -1,6 +1,8 @@
 import styled from "styled-components/native";
 import { colors } from "../../../infrastructure/theme/colors";
 
+//Initial Account Screen
+
 export const SwiperContainerStyles = styled.View`
   flex: 1;
   justify-content: center;
@@ -12,17 +14,38 @@ export const ButtonsContainer = styled.View`
   margin: 0px 15px;
 `;
 
-export const SignUpButton = styled.TouchableOpacity`
+export const ContainedButton = styled.TouchableOpacity`
   background-color: ${colors.brand.primary};
   border-radius: 30px;
   align-items: center;
-  padding: 12px;
+  padding: 13px;
 `;
 
-export const LogInButton = styled.TouchableOpacity`
+export const OutlinedButton = styled.TouchableOpacity`
   background-color: ${colors.ui.quaternary};
   border-radius: 30px;
   border: 1px solid ${colors.brand.primary};
   align-items: center;
-  padding: 12px;
+  padding: 13px;
+`;
+
+//Login Page Screen
+
+export const LoginContainer = styled.View`
+  flex: 1;
+  background-color: white;
+  padding: 0px 25px;
+`;
+
+export const LoginIconContainer = styled.View`
+  /* flex-direction: row; */
+  /* background-color: gray; */
+  padding: 30px 0px 10px 0px;
+  margin-left: -3px;
+`;
+
+export const TextContainer = styled.View`
+  flex-direction: row;
+  justify-content: center;
+  margin-bottom: 40px;
 `;
