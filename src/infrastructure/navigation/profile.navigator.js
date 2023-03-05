2de2305de9c75f7290da306f12b43c12d7415505
dedanmsafari <dedan.msafari@gmail.com>
2023-03-05 23:09:36 +0300

    profile Navigator

---
+++ src/infrastructure/navigation/profile.navigator.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const ProfileNavigator = () => {
+  return (
+    <View>
+      <Text>Profile Navigator</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ProfileNavigator;
