4bbae135ee9389e4f5cfb08457cafa751a471cb9
dedanmsafari <dedan.msafari@gmail.com>
2023-03-05 23:09:24 +0300

    home navigator

---
+++ src/infrastructure/navigation/home.navigator.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const HomeNavigator = () => {
+  return (
+    <View>
+      <Text>Home Navigator</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default HomeNavigator;
