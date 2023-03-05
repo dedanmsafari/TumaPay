9775acbb147930d2e7f941b7703682e4b3209b9a
dedanmsafari <dedan.msafari@gmail.com>
2023-03-05 23:16:43 +0300

    userDetails screen

--- src/features/Account/screens/userDetails.screens.js
+++ src/features/Account/screens/userDetails.screens.js
@@ -14,31 +14,34 @@ import Spacer from "../../../components/spacer/spacer.component";
 const UserDetailsScreen = ({ navigation }) => {
   return (
     <View style={{ flex: 1, backgroundColor: colors.ui.quaternary }}>
-      <StyledSafeAreaView>
-        <LoginContainer>
-          <BackArrow
-            title="Create Your Account."
-            caption="Enter your email address and password to setup  your account."
-          />
-          <Spacer size="medium" />
+      <LoginContainer>
+        <BackArrow
+          title="Create Your Account."
+          caption="Enter your email address and password to setup  your account."
+          step="Step 1 0f 2"
+        />
+        <Spacer size="medium" />
 
-          <TextInput label="Full Name" />
-          <TextInput label="Email address" keyboardType="email-address" />
-          <Spacer size="large" />
-          <ContainedButton activeOpacity={0.5}>
-            <Text variant="button">Continue</Text>
-          </ContainedButton>
-        </LoginContainer>
-        <TextContainer>
-          <Text variant="hint">Already Have an Account?</Text>
-          <TouchableOpacity onPress={() => navigation.navigate("Login")}>
-            <Text variant="button" inverted>
-              {" "}
-              Log In
-            </Text>
-          </TouchableOpacity>
-        </TextContainer>
-      </StyledSafeAreaView>
+        <TextInput label="Full Name" />
+        <TextInput label="Email address" keyboardType="email-address" />
+
+        <Spacer size="large" />
+        <ContainedButton
+          activeOpacity={0.5}
+          onPress={() => navigation.navigate("UserSignUp")}
+        >
+          <Text variant="button">Continue</Text>
+        </ContainedButton>
+      </LoginContainer>
+      <TextContainer>
+        <Text variant="hint">Already Have an Account?</Text>
+        <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+          <Text variant="button" inverted>
+            {" "}
+            Log In
+          </Text>
+        </TouchableOpacity>
+      </TextContainer>
     </View>
   );
 };
