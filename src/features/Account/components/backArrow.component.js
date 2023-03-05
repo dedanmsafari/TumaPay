f64a19d99be2f6c1519ca8c71e727a7e131ac3b0
dedanmsafari <dedan.msafari@gmail.com>
2023-03-05 23:17:40 +0300

    back arrow

--- src/features/Account/components/backArrow.component.js
+++ src/features/Account/components/backArrow.component.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { TouchableOpacity } from "react-native";
-import { LoginIconContainer } from "../screens/account.styles";
+import { LoginIconContainer, TitleContainer } from "../screens/account.styles";
 import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import Spacer from "../../../components/spacer/spacer.component";
 import Text from "../../../components/text/text.component";
 import { colors } from "../../../infrastructure/theme/colors";
 
-const BackArrow = ({ title, caption }) => {
+const BackArrow = ({ title, caption, step }) => {
   const navigation = useNavigation();
 
   return (
@@ -18,7 +18,10 @@ const BackArrow = ({ title, caption }) => {
         </TouchableOpacity>
       </LoginIconContainer>
       <Spacer size="medium" />
-      <Text variant="title">{title}</Text>
+      <TitleContainer>
+        <Text variant="title">{title}</Text>
+        {step && <Text variant="steps">{step}</Text>}
+      </TitleContainer>
       <Spacer size="medium" />
       {caption && <Text variant="hint">{caption}</Text>}
       <Spacer size="medium" />
