e6198aeba24c3420c312e61daaeae98581f79636
dedanmsafari <dedan.msafari@gmail.com>
2023-03-05 23:16:16 +0300

    wallet navigator

---
+++ src/infrastructure/navigation/wallet.navigator.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const WalletNavigator = () => {
+  return (
+    <View>
+      <Text>Wallet Navigator</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default WalletNavigator;
