37dd9b8fb5eef8b79166755fafe43f65e3b24201
dedanmsafari <dedan.msafari@gmail.com>
2023-03-06 10:41:29 +0300

    transfer screen added

---
+++ src/features/Transfers/screens/transfer.screens.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const TransferScreen = () => {
+  return (
+    <View>
+      <Text>Transfer Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default TransferScreen;
