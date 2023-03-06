5b9f8f882867ada0f952d0fc1291907589cb1ac8
dedanmsafari <dedan.msafari@gmail.com>
2023-03-06 10:41:07 +0300

    home styles added

---
+++ src/features/Home/screens/home.styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components/native";
+import { colors } from "../../../infrastructure/theme/colors";
+
+export const ProfileTab = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+
+  align-items: center;
+`;
+export const TextContainer = styled.View``;
+export const IconContainer = styled.View``;
