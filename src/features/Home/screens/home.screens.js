b0d079a42d137797596330eec3b96048d73fa518
dedanmsafari <dedan.msafari@gmail.com>
2023-03-07 15:44:19 +0300

    home screen

--- src/features/Home/screens/home.screens.js
+++ src/features/Home/screens/home.screens.js
@@ -3,8 +3,11 @@ import { View, StyleSheet, Image } from "react-native";
 import { ProfileTab, TextContainer, IconContainer } from "./home.styles";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import Text from "../../../components/text/text.component";
-import Spacer from "../../../components/text/text.component";
+import Spacer from "../../../components/spacer/spacer.component";
 import { colors } from "../../../infrastructure/theme/colors";
+import HeroBlock from "../components/hero.component";
+import EmptyTransaction from "../components/emptyTransaction.component";
+import TransactionElement from "../components/transactionElement.component";
 
 const HomeScreen = () => {
   return (
@@ -13,7 +16,7 @@ const HomeScreen = () => {
         style={{ height: 55, width: 150 }}
         source={require("../../../../assets/icon.png")}
       />
-      <Spacer size="xlarge" />
+      <Spacer size="medium" />
       <ProfileTab>
         <TextContainer>
           <Text variant="salutation">Good Morning,</Text>
@@ -27,10 +30,19 @@ const HomeScreen = () => {
           />
         </IconContainer>
       </ProfileTab>
+      <Spacer size="medium" />
+
+      <HeroBlock />
+      <Spacer size="medium">
+        <Text variant="label">Recent Transactions</Text>
+      </Spacer>
+      <Spacer size="medium" />
+      {/* <EmptyTransaction /> */}
+      <TransactionElement />
+      <TransactionElement />
+      <TransactionElement />
     </View>
   );
 };
-
-const styles = StyleSheet.create({});
 
 export default HomeScreen;
