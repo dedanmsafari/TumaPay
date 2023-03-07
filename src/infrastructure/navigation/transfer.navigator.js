622367a113557f9706a61e7e21f475eaae30c3ee
dedanmsafari <dedan.msafari@gmail.com>
2023-03-07 15:45:59 +0300

    transfer Navigator

--- src/infrastructure/navigation/transfer.navigator.js
+++ src/infrastructure/navigation/transfer.navigator.js
@@ -1,14 +1,45 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View } from "react-native";
+import {
+  createStackNavigator,
+  CardStyleInterpolators,
+} from "@react-navigation/stack";
+import TransferScreen from "../../features/Transfers/screens/transfer.screens";
+import BeneficiaryScreen from "../../features/Transfers/screens/beneficiary.screens";
+
+import { colors } from "../theme/colors";
+import { fonts } from "../theme/fonts";
+
+const Stack = createStackNavigator();
 
 const TransferNavigator = () => {
   return (
-    <View>
-      <Text>TransferNavigator</Text>
+    <View style={{ flex: 1, backgroundColor: "white" }}>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: true,
+          headerTitleAlign: "center",
+          headerTintColor: colors.brand.primary,
+          headerTitleStyle: {
+            fontFamily: fonts.body,
+            fontSize: 18,
+            fontWeight: 700,
+          },
+          headerStyle: {
+            height: 90, // set the height of the header
+          },
+          gestureEnabled: true,
+          cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+        }}
+      >
+        <Stack.Screen name="Send Money" component={TransferScreen} />
+        <Stack.Screen
+          name="Beneficiary Details"
+          component={BeneficiaryScreen}
+        />
+      </Stack.Navigator>
     </View>
   );
 };
-
-const styles = StyleSheet.create({});
 
 export default TransferNavigator;
