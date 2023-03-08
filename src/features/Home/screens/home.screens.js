aba2fc564164489921d40e225be1f9c80f30139f
dedanmsafari <dedan.msafari@gmail.com>
2023-03-08 16:54:55 +0300

    home screen

--- src/features/Home/screens/home.screens.js
+++ src/features/Home/screens/home.screens.js
@@ -8,39 +8,55 @@ import { colors } from "../../../infrastructure/theme/colors";
 import HeroBlock from "../components/hero.component";
 import EmptyTransaction from "../components/emptyTransaction.component";
 import TransactionElement from "../components/transactionElement.component";
+import styled from "styled-components/native";
+import { useNavigation } from "@react-navigation/native";
+import { StyledSafeAreaView } from "../../../utils/safeArea.util.component";
+
+const HomeSafeAreaView = styled(StyledSafeAreaView)`
+  margin-left: ${({ theme }) => theme.space[3]};
+  margin-right: ${({ theme }) => theme.space[2]};
+  padding-top: ${({ theme }) => theme.space[4]};
+  background-color: ${({ theme }) => theme.colors.bg.primary};
+`;
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
   return (
     <View style={{ flex: 1, backgroundColor: "white" }}>
-      <Image
-        style={{ height: 55, width: 150 }}
-        source={require("../../../../assets/icon.png")}
-      />
-      <Spacer size="medium" />
-      <ProfileTab>
-        <TextContainer>
-          <Text variant="salutation">Good Morning,</Text>
-          <Text variant="salutationcaption">Rob Skiba</Text>
-        </TextContainer>
-        <IconContainer>
-          <MaterialCommunityIcons
-            name="account-circle"
-            size={50}
-            color={colors.brand.primary}
-          />
-        </IconContainer>
-      </ProfileTab>
-      <Spacer size="medium" />
+      <HomeSafeAreaView>
+        <Image
+          style={{ height: 55, width: 150 }}
+          source={require("../../../../assets/icon.png")}
+        />
+        <Spacer size="medium" />
+        <ProfileTab>
+          <TextContainer>
+            <Text variant="salutation">Good Morning,</Text>
+            <Text variant="salutationcaption">Rob Skiba</Text>
+          </TextContainer>
+          <IconContainer
+            activeOpacity={0.5}
+            onPress={() => navigation.navigate("ProfileScreen")}
+          >
+            <MaterialCommunityIcons
+              name="account-circle"
+              size={50}
+              color={colors.brand.primary}
+            />
+          </IconContainer>
+        </ProfileTab>
+        <Spacer size="medium" />
 
-      <HeroBlock />
-      <Spacer size="medium">
-        <Text variant="label">Recent Transactions</Text>
-      </Spacer>
-      <Spacer size="medium" />
-      {/* <EmptyTransaction /> */}
-      <TransactionElement />
-      <TransactionElement />
-      <TransactionElement />
+        <HeroBlock navigation={navigation} />
+        <Spacer size="medium">
+          <Text variant="label">Recent Transactions</Text>
+        </Spacer>
+        <Spacer size="medium" />
+        {/* <EmptyTransaction /> */}
+        <TransactionElement />
+        <TransactionElement />
+        <TransactionElement />
+      </HomeSafeAreaView>
     </View>
   );
 };
