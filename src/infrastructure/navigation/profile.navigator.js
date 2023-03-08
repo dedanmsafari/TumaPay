3cdfc27efc25c39d399899b5c7bef9a06504a492
dedanmsafari <dedan.msafari@gmail.com>
2023-03-08 16:55:52 +0300

    profile Navigator

--- src/infrastructure/navigation/profile.navigator.js
+++ src/infrastructure/navigation/profile.navigator.js
@@ -1,14 +1,40 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View } from "react-native";
+import {
+  createStackNavigator,
+  CardStyleInterpolators,
+} from "@react-navigation/stack";
+import ProfileScreen from "../../features/Profile/screens/profile.screens";
+
+import { colors } from "../theme/colors";
+import { fonts } from "../theme/fonts";
+
+const Stack = createStackNavigator();
 
 const ProfileNavigator = () => {
   return (
-    <View>
-      <Text>Profile Navigator</Text>
+    <View style={{ flex: 1, backgroundColor: "white" }}>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: true,
+          headerTitleAlign: "center",
+          headerTintColor: colors.brand.primary,
+          headerTitleStyle: {
+            fontFamily: fonts.body,
+            fontSize: 18,
+            fontWeight: 700,
+          },
+          headerStyle: {
+            height: 90, // set the height of the header
+          },
+          gestureEnabled: true,
+          cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+        }}
+      >
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
     </View>
   );
 };
-
-const styles = StyleSheet.create({});
 
 export default ProfileNavigator;
